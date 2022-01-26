6b3377e52570aad984222f5c7d67d7fe06842524
Burhan <burhan.u6@hotmail.co.uk>
2022-01-26 19:12:04 +0000

    Fibonacci sequence w/ custom number of iterations/recursion

--- js-basics/fibonacciSequence.js
+++ js-basics/fibonacciSequence.js
@@ -1,21 +1,16 @@
-function fibonacci(a = 0  ,b = 1,iterations = 10) {
+function fibonacci(a) {
 
-    console.log(a)
-    console.log(b)
-    // for whatever start number we need to know the fib num from before
-    //e.g fib(a)
-
-  for (let i = 0; i < iterations; i++) { // controls number of iterations
-    var c = a+b
-    console.log(c);
-    a = b; 
-    b = c; 
-
-  }
-  return c;
+   if(a < 2)
+       return a
+   
+   else return fibonacci(a-1) + fibonacci(a-2);
 }
 
-fibonacci(1,1 ,undefined );
+nTerms = 10;
 
+
+    for(let i = 0; i < nTerms; i++) {
+        console.log(fibonacci(i));
+    }
 
 // fib nums 0,1,1,2,3,5,8,13,21,34...
